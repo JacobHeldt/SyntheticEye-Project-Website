3c716a9bb33591bfa27d8bcf34e3ad82d7d45246
Jacob Heldt <jacobemil.heldt@gmail.com>
2023-06-18 11:16:49 -0400

    content

---
+++ client/src/constants/index.js
@@ -0,0 +1,61 @@
+// All static Text shouwn on website
+import { imgslider1, imgslider2, imgslider3} from "../assets";
+
+
+// Navbar items
+export const navLinks = [
+    {
+        id: "home",
+        title: "Home"
+    },
+    {
+        id: "project",
+        title: "Project"
+    },
+    {
+        id: "about us",
+        title: "About us"
+    },
+    {
+        id: "models",
+        title: "Models"
+    }
+]
+
+
+// Our project
+export const project = [
+    {
+        id: "goal",
+        bg: "",
+        title: "Our Goal",
+        text: "Our goal is to develop a comprehensive application that detects and distinguishes AI-generated content from human-generated content across various media formats, starting with images and expanding to video, text, and audio. Thus enabling anyone to recognize AI-generated content and combat the spread of misinformation in the digital world."
+    },
+    {
+        id: "finalProduct",
+        bg: "",
+        text: "We want to develop an advanced and user-friendly application, that has the ability to easily detect AI-generated content. It will incorporate cutting-edge detection models and provide a seamless experience for users, enabling them to make informed decisions."
+    },
+    {
+        id: "stepsInvolved",
+        bg: "",
+        text: "Specialized models are built for images, videos, text, and audio using machine learning and deep learning methods. Diverse datasets are acquired and utilized to train the detection models, enhancing their accuracy and reliability."
+    }
+]
+
+
+// About Us
+export const imageSlider = [
+    {
+        id: "img-1",
+        source: imgslider1
+    },
+    {
+        id: "img-2",
+        source: imgslider2
+    },
+    {
+        id: "img-3",
+        source: imgslider3
+    }
+]
