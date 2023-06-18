c82fe0f4329c1d2b2a52d938c11269225329a636
Jacob Heldt <jacobemil.heldt@gmail.com>
2023-06-18 18:18:07 -0400

    hero section

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import styles from '../style'
 
-const Button = () => {
+const Button = ({text}) => {
   return (
-    <div>Button</div>
+    <button className='primary-gradient tracking-wider font-righteous  px-10 py-3 text-lg font- uppercase rounded-md hover:rounded-lg'>{text}</button>
   )
 }
 
+Button.propTypes = {
+  text: PropTypes.string.isRequired,
+}
 export default Button
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -26,7 +26,18 @@ const Navbar = () => {
       <div className="sm:hidden flex flex-1 justify-end items-center">
         <img src={toggle ? close : menu} alt="menu" className='w-[28px] text-blue-500' onClick={() => setToggle((prev) => !prev)}/>
 
-        <div className=''>
+        <div className={`${
+            !toggle ? "hidden" : "flex"
+          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}>
+        <ul className='list-none flex-col justify-end items-start flex-1'>
+        {navLinks.map((nav, index) => (
+          <li key={nav.id} className={`font-poppins mb-4 font-normal cursor-pointer text-base text-white ' ${index === navLinks.length - 1 ? 'mb-0' : 'mb-4'}`}>
+            <a href={`#${nav.id}`}>
+              {nav.title}
+            </a>
+          </li>
+        ))}
+      </ul>
 
         </div>
 
