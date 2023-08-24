51e10b4c0c267ce5bf03f6bb58b6d39aee003044
Jacob Heldt <jacobemil.heldt@gmail.com>
2023-08-24 11:50:32 -0400

    Setup gunicorn

--- backend/app.py
+++ backend/app.py
@@ -141,6 +141,3 @@ def predict():
         else:
             return jsonify({"error": "Invalid file type"}), 400
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
-    
